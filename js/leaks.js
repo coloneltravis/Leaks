1eb999bb8f38ec19e63c3d8ed8fb5d393ca2c578
Jake Bourne <jake.bourne@gmail.com>
2014-11-08 17:39:36 +0000

    Update leaks.js

--- js/leaks.js
+++ js/leaks.js
@@ -1,25 +1,25 @@
-var leaks = [0, 0, 0, 0, 0, 0];
+var drops = [0, 0, 0, 0, 0, 0];
 
-var bucket = 0;
+var bucket = {pos:3, level:0};
 
 var barrel1 = 0;
 var barrel2 = 0;
 
-var bucket_size = 5, barrel_size = 100;
+var bucket_size = 4, barrel_size = 100;
 
 
 var drop_interval = 10;
 
 var gameloop = 0;
-
+var timer;
 
 $(document).ready(function(e) {
-	$(document).keydown(function(e) {keyPressed(e.keyCode); });
+	$(window).keydown(function(e) {keyPressed(e.keyCode); });
 
-	var ctx = $('#maincanvas')[0].getContext('2d');
+	//var ctx = $('#maincanvas')[0].getContext('2d');
 
 	gameloop = 0;
-	setInterval(onTimer(), 100);
+	timer = setInterval(onTimer, 100);
 });
 
 
@@ -28,14 +28,25 @@ function onTimer() {
 	if ((gameloop % drop_interval) == 0) {
 		// get random number between 1 and 5 to use as index to leaks array		
 		var leakpos = Math.floor(Math.random()*6);
-		leaks[leakpos]++;
-		
-		//alert('leak!');
+		drops[leakpos]++;
+		debug();
+
+		// when a leak reaches the floor, stop the game loop
+		for (i=0; i<drops.length; i++) {
+			
+			if (drops[i] == 9 && bucket.pos == i) {
+				bucket.level++;
+				drops[i] = 0;
+			}
+
+			if (drops[i] == 10) {
+				clearInterval(timer);
+			}
+		}		
 	}
 
 	gameloop++;
 
-	debug();
 
 	updatePanel1();
 
@@ -50,16 +61,28 @@ function keyPressed(key) {
 		//case 40 : alert('down');
 		//break;
 
-		case 37 : alert('move left');
+		case 37 : moveLeft();
 		break;
 		
-		case 39 : alert('move right');
+		case 39 : moveRight();
 		break;
 		
-		case 32 : alert('empty bucket');
+		case 32 : console.log('empty bucket');
 		break;								
 	}
 	
+}
+
+
+function moveLeft() {
+	bucket.pos--;
+	if (bucket.pos < 0) bucket.pos = 0;
+}
+
+
+function moveRight() {
+	bucket.pos++;
+	if (bucket.pos > 5) bucket.pos = 5;
 }
 
 
@@ -74,12 +97,6 @@ function updatePanel2() {
 
 
 function debug() {
-	$('#debug').empty();
-
-	var str = 'leaks: ';
-	for (var i=0; i<leaks.length; i++)
-		str += parseInt(leaks[i]) + ',';
-
-	$('#debug').append(str + '<br/>');
+	console.log(gameloop + ': DROPS : ' + drops);
+	console.log('BUCKET: ' + bucket.pos + ' : ' + bucket.level);
 }
-	
